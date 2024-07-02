e8862c7201ac28bf6194368f0cd28ddae264af78
BachoKartsivadze <bachoqarcivadze62@gmail.com>
2024-07-02 10:13:04 +0400

    make county table scrollable

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-interface Country {
-  name: string;
-  iso: string;
-  country_iso: string;
-  flag: string;
-}
+import { Country } from '../models/country';
 
 @Component({
   selector: 'app-countries',
@@ -13,16 +7,16 @@ interface Country {
   styleUrls: ['./countries.component.css'],
 })
 export class CountriesComponent implements OnInit {
-  countries: Country[] = [];
+  countries: Country[] = [
+    { name: 'GEO', iso: '00000', country_iso: '00000', flag: 'flag of GEO' },
+    { name: 'AZE', iso: '00001', country_iso: '00001', flag: 'flag of AZE' },
+    { name: 'UAE', iso: '00002', country_iso: '00002', flag: 'flag of UAE' },
+    { name: 'KAZ', iso: '00003', country_iso: '00003', flag: 'flag of KAZ' },
+    { name: 'TUR', iso: '00004', country_iso: '00004', flag: 'flag of TUR' },
+    { name: 'QAT', iso: '00005', country_iso: '00005', flag: 'flag of QAT' },
+  ];
 
-  ngOnInit(): void {
-    this.countries = [
-      { name: 'GEO', iso: '00000', country_iso: '00000', flag: 'flag of GEO' },
-      { name: 'AZE', iso: '00001', country_iso: '00001', flag: 'flag of AZE' },
-      { name: 'UAE', iso: '00002', country_iso: '00002', flag: 'flag of UAE' },
-      { name: 'KAZ', iso: '00003', country_iso: '00003', flag: 'flag of KAZ' },
-      { name: 'TUR', iso: '00004', country_iso: '00004', flag: 'flag of TUR' },
-      { name: 'QAT', iso: '00005', country_iso: '00005', flag: 'flag of QAT' },
-    ];
-  }
+  constructor() {}
+
+  ngOnInit(): void {}
 }
---
+++ src/app/models/country.ts
@@ -0,0 +1,6 @@
+export interface Country {
+  name: string;
+  iso: string;
+  country_iso: string;
+  flag: string;
+}
